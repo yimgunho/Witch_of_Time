9c6d382ba858c36803a767fd6e49b7fc7313a540
Yim Gunho <rjsgh2119@gmail.com>
2021-04-07 17:58:19 +0900

    새 파일들 추가

---
+++ Witch_of_Time/Source/Witch_of_Time/Widget_Command.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Widget_Command.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WITCH_OF_TIME_API UWidget_Command : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
